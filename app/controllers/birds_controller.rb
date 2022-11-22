1b5a6a54aac32c65a35e96ad952365b36e47ad06
Sylvia Ndili <ndilisylvia@gmail.com>
2022-11-22 11:23:09 +0300

    completed assignment

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,55 @@
+class BirdsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity_response
+  
+    # GET /birds
+    def index
+      birds = Bird.all
+      render json: birds
+    end
+  
+    # POST /birds
+    def create
+      bird = Bird.create!(bird_params)
+      render json: bird, status: :created
+    end
+  
+    # GET /birds/:id
+    def show
+      bird = find_bird
+      render json: bird
+    end
+  
+    # PATCH /birds/:id
+    def update
+      bird = find_bird
+      bird.update!(bird_params)
+      render json: bird
+    end
+  
+    # DELETE /birds/:id
+    def destroy
+      bird = find_bird
+      bird.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def find_bird
+      Bird.find(params[:id])
+    end
+  
+    def bird_params
+      params.permit(:name, :species, :likes)
+    end
+  
+    def render_not_found_response
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  
+    def render_unproccessable_entity_response(invalid)
+      render json: {errors: invalid.record.errors.full_messages }, status: 422
+    end
+  
+  end
